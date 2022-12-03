8fdc944eb88cb12eb295642f9d998f48abed4820
Bobi <bobak.gergo18@gmail.com>
2022-12-03 17:25:44 +0100

    Added simple search bar -Search to games in sql database -If no result no result response

--- inc/games.php
+++ inc/games.php
@@ -1,7 +1,7 @@
-    <?php 
+<?php 
       $result = mysqli_query($con,"SELECT * FROM games");
       $row = mysqli_fetch_assoc($result);
-     if(isset($_GET['game_id'])){
+if(isset($_GET['game_id'])){
       $game_result=mysqli_query($con,"SELECT * FROM games WHERE id=$_GET[game_id]");
       $game_row=mysqli_fetch_assoc($game_result);
       $gamepicture= mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM pictures WHERE id='$game_row[game_picture_id]'"));
@@ -57,25 +57,58 @@
       </div>
       </form>';
 }
-      print '
-      <div class="flex_game_about_conainer">
-            
-            <div class="game_infos"> 
-              <p>Genre: ';print $game_row["game_genre"];print ' </p>
-              <p>Relase date: ';print $game_row["game_relase_date"];print ' </p> 
-              <p>Platforms: ';print $game_row["game_platforms"];print ' </p> 
-              <p>Developers: ';print $game_row["game_developers"];print ' </p> 
-              <p>Game-modes: ';print $game_row["game_gamemodes"];print ' </p> 
-              <p>User rating: ';print $game_row["game_rating"];print '/10 </p> 
-           
-            <div class="game_rating"></div>
-         <h1>About</h1>
-        <div class="text_content"> <p>';print $game_row["game_about"];print ' </p> </div> 
-      </div>    
-';
+print '
+<div class="flex_game_about_conainer">
       
-     }
+      <div class="game_infos"> 
+        <p>Genre: ';print $game_row["game_genre"];print ' </p>
+        <p>Relase date: ';print $game_row["game_relase_date"];print ' </p> 
+        <p>Platforms: ';print $game_row["game_platforms"];print ' </p> 
+        <p>Developers: ';print $game_row["game_developers"];print ' </p> 
+        <p>Game-modes: ';print $game_row["game_gamemodes"];print ' </p> 
+        <p>User rating: ';print $game_row["game_rating"];print '/10 </p> 
+     
+      <div class="game_rating"></div>
+   <h1>About</h1>
+  <div class="text_content"> <p>';print $game_row["game_about"];print ' </p> </div> 
+</div>    
+';
+}
      else{
+      if(isset($_GET['search'])){
+        print'<h1>Search results</h1>
+      <div class="gamelistdiv">';
+      $result2 = mysqli_query($con,"SELECT * FROM games WHERE game_name LIKE '%$_GET[search]%'");
+      if( mysqli_num_rows($result2) == 0) print '
+      <div class="gamecontainer">
+      <div class="card" style="width: 14rem;">
+        <div class="card-body">
+          <h5 class="card-title" style="text-align: center;">Az ön álltal keresett kifejezés nem található.</h5></a>
+            <p class="card-text"></p>
+    </div>
+      </div>
+        </div>';
+      while($row2=mysqli_fetch_assoc($result2) and mysqli_num_rows($result2) != 0){
+        
+        $g_img = mysqli_query($con,"SELECT * FROM pictures WHERE id='$row2[game_picture_id]'");
+        $g_row = mysqli_fetch_assoc($g_img);
+        print '
+        
+          <div class="gamecontainer">
+            <div class="card" style="width: 14rem;">
+            <a href="index.php?oldal=games&game_id=';print $row2["id"] ;print '"><img class="card-img-top" src=';print $g_row["picture_path"];print ' alt="Card image cap" >
+          <div class="card-body">
+            <a href="index.php?oldal=games&game_id=';print $row2["id"] ;print '"><h5 class="card-title" style="text-align: center;">';print $row2["game_name"];print '</h5></a>
+            <p class="card-text">Relase date: <br> ';print $row2["game_relase_date"];print '<br>Rating: ';print $row2["game_rating"];print '/10 <br>';print $row2["game_platforms"];print ' </p>
+            <p class="card-text"></p>
+          </div>
+        </div>
+        </div>
+        ';
+      }
+      print '</div>';
+      }
+      else{
       print'<h1>Game List</h1>
       <div class="gamelistdiv">';
       $result2 = mysqli_query($con,"SELECT * FROM games");
@@ -98,7 +131,7 @@
         ';
       }
       print '</div>';
-     }
+     }}
 
     ?>
     
